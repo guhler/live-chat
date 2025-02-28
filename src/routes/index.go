5fcaa537d06ed4658039b2b58933550d4450cf07
Gideon Bühler <gideonbuehler18@gmail.com>
2025-02-28 16:30:04 +0100

    redirect from index to /rooms

--- src/routes/index.go
+++ src/routes/index.go
@@ -2,24 +2,19 @@ package routes
 
 import (
 	"database/sql"
-	"live_chat/util"
+	"live_chat/auth"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-func GetIndex(db *sql.DB) (string, string, echo.HandlerFunc) {
+func GetIndex(db *sql.DB) (string, string, echo.HandlerFunc, echo.MiddlewareFunc) {
 	return "GET", "/", func(c echo.Context) error {
-		userName, ok := c.Get("authorized_user").(string)
+		_, ok := c.Get("authorized_user").(string)
 		if !ok {
 			return c.Render(http.StatusOK, "index.html", nil)
 		}
 
-		roomNames, err := util.GetRoomsOfUser(db, userName)
-		if err != nil {
-			return err
-		}
-
-		return c.Render(http.StatusOK, "index_auth.html", indexPage{roomNames})
-	}
+		return c.Redirect(http.StatusFound, "/rooms")
+	}, auth.TokenParser
 }
